13c63edb2f2537288db908fab2a926cfb8f49ddc
simtropbuggi <simtropbuggi@ed9f5d1b-00bb-0f29-faab-e8ebad1a710c>
2013-09-11 06:25:18 +0000

    made block a booster for temporary testing purposes.

--- trunk/powercraft_16/forge/mcp/src/minecraft/powercraft/transport/blocks/blocks/PCtr_BlockUpgradeableBelt.java
+++ trunk/powercraft_16/forge/mcp/src/minecraft/powercraft/transport/blocks/blocks/PCtr_BlockUpgradeableBelt.java
@@ -1,70 +1,108 @@
 package powercraft.transport.blocks.blocks;
 
-import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import powercraft.api.blocks.PC_BlockRotated;
+import powercraft.api.PC_Direction;
+import powercraft.api.PC_Vec3;
+import powercraft.api.blocks.PC_BlockInfo;
+import powercraft.api.blocks.PC_BlockWithoutTileEntity;
+import powercraft.api.registries.PC_TextureRegistry;
 import powercraft.transport.helper.PCtr_BeltHelper;
 import powercraft.transport.helper.PCtr_MaterialConveyor;
 
-public class PCtr_BlockUpgradeableBelt extends PC_BlockRotated {
+@PC_BlockInfo(name = "UpgradableBelt", blockid = "upgradableBelt", defaultid = 2051)
+public class PCtr_BlockUpgradeableBelt extends PC_BlockWithoutTileEntity
+{
 
-	public PCtr_BlockUpgradeableBelt(int id) {
+	public PCtr_BlockUpgradeableBelt(int id)
+	{
 		super(id, PCtr_MaterialConveyor.getMaterial());
 		setCreativeTab(CreativeTabs.tabBlock);
 	}
 
 	@Override
-	public void loadIcons() {
+	public void loadIcons()
+	{
+		this.blockIcon = PC_TextureRegistry.registerIcon("upgradeablebelt");
+
+	}
+
+	@Override
+	public void registerRecipes()
+	{
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void registerRecipes() {
-		// TODO Auto-generated method stub
+	public void onEntityCollidedWithBlock(World world, int i, int j, int k, Entity entity)
+	{
+		if (!entity.onGround) return;
+		PC_Vec3 emotion = new PC_Vec3(entity.motionX, entity.motionY, entity.motionZ);
+		PC_Direction eDir = PCtr_BeltHelper.returnPrimaryDirection(emotion, true);
+		switch (eDir)
+		{
+			case NORTH:
+			case SOUTH:
+				entity.motionZ *= 2;
+				break;
+			case EAST:
+			case WEST:
+				entity.motionX *= 2;
+			case DOWN:
+				break;
+			case UP:
+				break;				
+			case UNKNOWN:
+				break;
 
+			default:
+				break;
+		}
 	}
 
-	
 	@Override
-    public void onEntityCollidedWithBlock(World world, int i, int j, int k, Entity entity){
-		if(!entity.onGround) return;
-		
-    }
-    
-	@Override
-	public boolean isOpaqueCube() {
-	    return PCtr_BeltHelper.isOpaqueCube();
+	public boolean isOpaqueCube()
+	{
+		return PCtr_BeltHelper.isOpaqueCube();
 	}
+
 	@Override
-	public boolean renderAsNormalBlock() {
-	    return PCtr_BeltHelper.renderAsNormalBlock();
+	public boolean renderAsNormalBlock()
+	{
+		return PCtr_BeltHelper.renderAsNormalBlock();
 	}
+
 	@Override
-	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int i,
-			int j, int k) {
-			    return PCtr_BeltHelper.getCollisionBoundingBoxFromPool(world, i, j, k);
-			}
-	@Override
-	public AxisAlignedBB getSelectedBoundingBoxFromPool(World world, int i,
-			int j, int k) {
-		return PCtr_BeltHelper.getSelectedBoundingBoxFromPool(world, i,j,k);
+	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int i, int j, int k)
+	{
+		return PCtr_BeltHelper.getCollisionBoundingBoxFromPool(world, i, j, k);
 	}
+
 	@Override
-	public void setBlockBoundsBasedOnState(IBlockAccess iblockaccess, int i,
-			int j, int k) {
+	public AxisAlignedBB getSelectedBoundingBoxFromPool(World world, int i, int j, int k)
+	{
+		return PCtr_BeltHelper.getSelectedBoundingBoxFromPool(world, i, j, k);
+	}
+
+	@Override
+	public void setBlockBoundsBasedOnState(IBlockAccess iblockaccess, int i, int j, int k)
+	{
 		PCtr_BeltHelper.setBlockBoundsBasedOnState(this, iblockaccess, i, j, k);
-			}
+	}
+
 	@Override
-	public void setBlockBoundsForItemRender() {
+	public void setBlockBoundsForItemRender()
+	{
 		PCtr_BeltHelper.setBlockBoundsForItemRender(this);
 	}
+
 	@Override
-	public int tickRate(World world) {
-	    return PCtr_BeltHelper.tickRate(world);
+	public int tickRate(World world)
+	{
+		return PCtr_BeltHelper.tickRate(world);
 	}
 }
